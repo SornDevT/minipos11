a38d8481aa27b5040238915bbbe950956d265518
SornDev <sorndev.teach@gmail.com>
2023-09-15 21:09:18 +0700

    install vue-router and create Tables... VDO 03

---
+++ resources/js/pages/Store.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Store Page 1234</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Minipos11Store',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
